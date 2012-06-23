907d04b07ee14d9ef2fcb1dd96bf27e4c3a68673
manarius <jascha@gmx.at>
2012-06-23 19:03:42 +0300

    should work if your madewith url is customized but not activated on madewith.meteor.com

--- client/js/stellarskeleton.js
+++ client/js/stellarskeleton.js
@@ -51,36 +51,37 @@ StellarSkeleton.load.madewith = function() {
 
     // minimongo collection to hold my singleton app record.
     var apps = new Meteor.Collection('madewith_apps', server);
-
-    server.methods({
-      vote: function (hostname) {
-        apps.update({name: hostname}, {$inc: {vote_count: 1}});
-      }
-    });
-
-    Template.madewith.vote_count = function() {
-      var app = apps.findOne();
-      return app ? app.vote_count : '???';
-    };
-
-    Template.madewith.shortname = function () {
-      return madewith.value;
-    };
-
-    Template.madewith.events = {
-      'click .madewith_upvote': function(event) {
-        var app = apps.findOne();
-        if (app) {
-          server.call('vote', hostname);
-          // stop these so you don't click through the link to go to the
-          // app.
+    if(apps){
+      server.methods({
+        vote: function (hostname) {
+          apps.update({name: hostname}, {$inc: {vote_count: 1}});
         }
-        
-        event.stopPropagation();
-        event.preventDefault();
-      }
-    };
-
-    $('body').append(Meteor.ui.render(function() { return Template.madewith();}));
+      });
+  
+      Template.madewith.vote_count = function() {
+        var app = apps.findOne();
+        return app ? app.vote_count : '???';
+      };
+  
+      Template.madewith.shortname = function () {
+        return madewith.value;
+      };
+  
+      Template.madewith.events = {
+        'click .madewith_upvote': function(event) {
+          var app = apps.findOne();
+          if (app) {
+            server.call('vote', hostname);
+            // stop these so you don't click through the link to go to the
+            // app.
+          }
+          
+          event.stopPropagation();
+          event.preventDefault();
+        }
+      };
+  
+      $('body').append(Meteor.ui.render(function() { return Template.madewith();}));
+    }
   }
 }
